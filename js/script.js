ea9558afe9792e6a35d00825f96c0d2e6268919e
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-24 17:44:05 +0100

    [feat] foundamental human code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 console.log('lo script funziona');
+
+// l'api ci ritorna dei dati
+/*
+è un array di oggetti contenente queste proprietà:
+id, titolo, data, url 
+*/
+
+// compito principale
+// il mio scopo è stamparli a schermo insieme al codice html
+
+// --- PRIMA PARTE
+// COSA MI SERVE
+// Dati del DOM da riempire
+// chiamata all'endpoint 
+// estrapolazione dei dati tramite ciclo
+// inserimento dei dati nel codice html
+
+
+// compito secondario
+// al click dell'immagine deve apparirmi la stessa immagine ingrandita
+
+// --- SECONDA PARTE
+// COSA MI SERVE
+// dati del DOM da gestire--> contenitore, pulsante
+// al click dell'immagine
+// mostra la foto 
+// al click del pulsante chiudi, riporta in d-none 
+
+
+
